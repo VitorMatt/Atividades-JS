e48ae913d18e29bcb0959bbaf3bcfd80f1c48c77
Vitor :) <vitor.azevedoo2812@gmail.com>
2024-06-24 15:21:40 -0300

    Delete Atividade 13 lista 1 JavaScript.js

--- Atividade 13 lista 1 JavaScript.js
+++
@@ -1,7 +0,0 @@
-/*13) Uma feira de livros está fazendo promoção onde na compra de 3 livros, o(a)
-comprador(a) ganha 15% de desconto. Criar um programa que receba os valores dos 3
-livros e mostra na tela o total dos livros sem desconto e com desconto*/
-
-const desconto = 15
-var valorTotalComDesconto
-var 
